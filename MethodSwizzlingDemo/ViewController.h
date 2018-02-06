774b12fa26003e0c52dcd7b67056799f1e34a192
zhangzr <zhangzr@vastio.com>
2018-02-06 14:32:25 +0800

    Initial Commit

---
+++ MethodSwizzlingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MethodSwizzlingDemo
+//
+//  Created by 张忠瑞 on 2018/2/6.
+//  Copyright © 2018年 张忠瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MethodSwizzlingDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MethodSwizzlingDemo
+//
+//  Created by 张忠瑞 on 2018/2/6.
+//  Copyright © 2018年 张忠瑞. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
